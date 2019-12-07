03a6e306669ce3aa1d2d5a45c1cbc172ba8a6b1e
Thiago Capelo <thiagokpelo@gmail.com>
2019-12-06 23:27:57 -0300

    add: reset styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from 'store';
+import { Reset } from 'theme';
 import RepositoryList from 'components/RepositoryList';
 import { Header, Logo, Wrapper } from './styles';
 import logo from './logo.svg';
@@ -8,6 +9,7 @@ import logo from './logo.svg';
 const App: React.FC = () => (
   <Provider store={store}>
     <Wrapper>
+      <Reset />
       <Header>
         <Logo src={logo} className="App-logo" alt="logo" />
         <RepositoryList />
